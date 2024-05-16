aa261a475d4b190e825028d4ceb990c6c031e363
Samuel Sihotang <samsihotang1@gmail.com>
2024-05-16 20:59:24 +0700

    homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+const ScriptLoader = ({ src, onLoad }: { src: string; onLoad: () => void }) => {
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src = src;
+		script.async = true;
+		script.onload = onLoad;
+		document.body.appendChild(script);
+
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, [src, onLoad]);
+
+	return null;
+};
+
+const Main = () => {
+	const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+	const handleScriptLoad = () => {
+		setIsScriptLoaded(true);
+	};
+
+	return (
+		<>
+			{!isScriptLoaded && (
+				<ScriptLoader
+					src="/assets/js/common_scripts.min.js"
+					onLoad={handleScriptLoad}
+				/>
+			)}
+			{isScriptLoaded && <App />}
+		</>
+	);
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Main />
+	</React.StrictMode>
+);
