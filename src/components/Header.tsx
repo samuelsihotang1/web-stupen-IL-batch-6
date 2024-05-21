b888aacecdebe315a284cc404db4f9332b75f72e
Samuel Sihotang <samsihotang1@gmail.com>
2024-05-21 15:35:41 +0700

    Update header navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,236 +52,20 @@ function Header() {
 											<a href="##" className="show-submenu">
 												Home
 											</a>
-											<ul>
-												<li>
-													<a href="index.html">Slider</a>
-												</li>
-												<li>
-													<a href="index-2.html">
-														Video Background
-													</a>
-												</li>
-												<li>
-													<a href="index-3.html">
-														Vertical Slider
-													</a>
-												</li>
-												<li>
-													<a href="index-4.html">
-														GDPR Cookie Bar
-													</a>
-												</li>
-											</ul>
 										</li>
 										<li className="megamenu submenu">
 											<a href="##" className="show-submenu-mega">
-												Pages
+												Artikel
 											</a>
-											<div className="menu-wrapper">
-												<div className="row small-gutters">
-													<div className="col-lg-3">
-														<h3>Listing grid</h3>
-														<ul>
-															<li>
-																<a href="listing-grid-1-full.html">
-																	Grid Full Width
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-2-full.html">
-																	Grid Full Width 2
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-3.html">
-																	Grid Boxed
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-4-sidebar-left.html">
-																	Grid Sidebar Left
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-5-sidebar-right.html">
-																	Grid Sidebar Right
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-6-sidebar-left.html">
-																	Grid Sidebar Left 2
-																</a>
-															</li>
-															<li>
-																<a href="listing-grid-7-sidebar-right.html">
-																	Grid Sidebar Right 2
-																</a>
-															</li>
-														</ul>
-													</div>
-													<div className="col-lg-3">
-														<h3>Listing row &amp; Product</h3>
-														<ul>
-															<li>
-																<a href="listing-row-1-sidebar-left.html">
-																	Row Sidebar Left
-																</a>
-															</li>
-															<li>
-																<a href="listing-row-2-sidebar-right.html">
-																	Row Sidebar Right
-																</a>
-															</li>
-															<li>
-																<a href="listing-row-3-sidebar-left.html">
-																	Row Sidebar Left 2
-																</a>
-															</li>
-															<li>
-																<a href="listing-row-4-sidebar-extended.html">
-																	Row Sidebar Extended
-																</a>
-															</li>
-															<li>
-																<a href="product-detail-1.html">
-																	Product Large Image
-																</a>
-															</li>
-															<li>
-																<a href="product-detail-2.html">
-																	Product Carousel
-																</a>
-															</li>
-															<li>
-																<a href="product-detail-3.html">
-																	Product Sticky Info
-																</a>
-															</li>
-														</ul>
-													</div>
-													<div className="col-lg-3">
-														<h3>Other pages</h3>
-														<ul>
-															<li>
-																<a href="cart.html">
-																	Cart Page
-																</a>
-															</li>
-															<li>
-																<a href="checkout.html">
-																	Check Out Page
-																</a>
-															</li>
-															<li>
-																<a href="confirm.html">
-																	Confirm Purchase Page
-																</a>
-															</li>
-															<li>
-																<a href="account.html">
-																	Create Account Page
-																</a>
-															</li>
-															<li>
-																<a href="track-order.html">
-																	Track Order
-																</a>
-															</li>
-															<li>
-																<a href="help.html">
-																	Help Page
-																</a>
-															</li>
-															<li>
-																<a href="help-2.html">
-																	Help Page 2
-																</a>
-															</li>
-															<li>
-																<a href="leave-review.html">
-																	Leave a Review
-																</a>
-															</li>
-														</ul>
-													</div>
-													<div className="col-lg-3 d-xl-block d-lg-block d-md-none d-sm-none d-none">
-														<div className="banner_menu">
-															<a href="#0">
-																<img
-																	src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-																	data-src="/assets/img/banner_menu.jpg"
-																	width="400"
-																	height="550"
-																	alt=""
-																	className="img-fluid lazy"
-																/>
-															</a>
-														</div>
-													</div>
-												</div>
-												{/* <!-- /row --> */}
-											</div>
 											{/* <!-- /menu-wrapper --> */}
 										</li>
 										<li className="submenu">
 											<a href="##" className="show-submenu">
-												Extra Pages
+												Produk
 											</a>
-											<ul>
-												<li>
-													<a href="header-2.html">
-														Header Style 2
-													</a>
-												</li>
-												<li>
-													<a href="header-3.html">
-														Header Style 3
-													</a>
-												</li>
-												<li>
-													<a href="header-4.html">
-														Header Style 4
-													</a>
-												</li>
-												<li>
-													<a href="header-5.html">
-														Header Style 5
-													</a>
-												</li>
-												<li>
-													<a href="404.html">404 Page</a>
-												</li>
-												<li>
-													<a href="sign-in-modal.html">
-														Sign In Modal
-													</a>
-												</li>
-												<li>
-													<a href="contacts.html">Contact Us</a>
-												</li>
-												<li>
-													<a href="about.html">About 1</a>
-												</li>
-												<li>
-													<a href="about-2.html">About 2</a>
-												</li>
-												<li>
-													<a href="modal-advertise.html">
-														Modal Advertise
-													</a>
-												</li>
-												<li>
-													<a href="modal-newsletter.html">
-														Modal Newsletter
-													</a>
-												</li>
-											</ul>
 										</li>
 										<li>
-											<a href="blog.html">Blog</a>
-										</li>
-										<li>
-											<a href="#0">Buy Template</a>
+											<a href="blog.html">Tentang</a>
 										</li>
 									</ul>
 								</div>
@@ -291,7 +75,7 @@ function Header() {
 								<div className="custom-search-input">
 									<input
 										type="text"
-										placeholder="Search over 10.000 products"
+										placeholder="Cari produk..."
 									/>
 									<button type="submit">
 										<i className="header-icon_search_custom"></i>
@@ -434,7 +218,7 @@ function Header() {
 						<input
 							type="text"
 							className="form-control"
-							placeholder="Search over 10.000 products"
+							placeholder="Cari produk..."
 						/>
 						<input
 							type="submit"
