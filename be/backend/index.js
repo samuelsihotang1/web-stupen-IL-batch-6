14e2f1679cb05cfa7dd665a28463a6f1b758f2ff
Musyaffa Afiful Umam <afzam613@gmail.com>
2024-06-08 22:08:54 +0700

    Done Backend
    min Edit in Frontend

---
+++ be/backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const routeProduk = require("./route/produkRoute");
+const routeArtikel = require("./route/artikelRoute");
+const routeUser = require("./route/userRoute");
+const routePesanan = require("./route/pesananRoute");
+
+const dotenv = require("dotenv");
+const { testConnection } = require("./database/Db");
+app.use(cors());
+dotenv.config();
+
+app.use(bodyparser.json());
+app.use(routeProduk);
+app.use(routeArtikel);
+app.use(routeUser);
+app.use(routePesanan);
+
+app.listen(process.env.APP_PORT, async () => {
+  await testConnection();
+  console.log(`Server running at http://localhost:${process.env.APP_PORT}`);
+});
---
+++ be/frontend/src/components/artikel/AddArtikel.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddArtikel = () => {
+  const [judul_artikel, setJudulArtikel] = useState("");
+  const [detail_artikel, setDetailArtikel] = useState("");
+  const navigate = useNavigate();
+
+  const saveArtikel = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/artikel", {
+        judul_artikel,
+        detail_artikel,
+      });
+      navigate("/listArtikel");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <form onSubmit={saveArtikel}>
+          <div className="field">
+            <label className="label">Judul Artikel</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={judul_artikel}
+                onChange={(e) => setJudulArtikel(e.target.value)}
+                placeholder="Judul Artikel"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Detail</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={detail_artikel}
+                onChange={(e) => setDetailArtikel(e.target.value)}
+                placeholder="Detail Artikel"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-success">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddArtikel;
