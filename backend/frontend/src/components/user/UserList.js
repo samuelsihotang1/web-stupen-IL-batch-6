8f564713ebdb0c295145f03179bc3e0026283587
Samuel Sihotang <samsihotang1@gmail.com>
2024-06-17 19:26:01 +0700

    factor

--- backend/frontend/src/components/user/UserList.js
+++ backend/frontend/src/components/user/UserList.js
@@ -1,78 +1,76 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const UserList = () => {
-  const [users, setUser] = useState([]);
+	const [users, setUser] = useState([]);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+	useEffect(() => {
+		getUsers();
+	}, []);
 
-  const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
-    setUser(response.data);
-  };
+	const getUsers = async () => {
+		const response = await axios.get('http://localhost:5000/users');
+		setUser(response.data);
+	};
 
-  const deleteUser = async (id) => {
-    try {
-      await axios.delete(`http://localhost:5000/users/${id}`);
-      getUsers();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const deleteUser = async (id) => {
+		try {
+			await axios.delete(`http://localhost:5000/users/${id}`);
+			getUsers();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className="columns mt-5">
-      <div className="column is-half">
-        <Link to={`/user/add`} className="button is-success">
-          Add New
-        </Link>
-        <table className="table is-striped is-fullwidth">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Adress</th>
-              <th>Phone</th>
-              <th>CreatedAt</th>
-              <th>UpdatedAt</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={user.id}>
-                <td>{index + 1}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.address}</td>
-                <td>{user.phone}</td>
-                <td>{user.created_at}</td>
-                <td>{user.updated_at}</td>
-                <td>
-                  <Link
-                    to={`/user/edit/${user.id}`}
-                    className="button is-small is-info mr-2"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    onClick={() => deleteUser(user.id)}
-                    className="button is-small is-danger"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+	return (
+		<div className="columns mt-5">
+			<div className="column is-half">
+				<Link to={`/user/add`} className="button is-success">
+					Add New
+				</Link>
+				<table className="table is-striped is-fullwidth">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Adress</th>
+							<th>Phone</th>
+							<th>CreatedAt</th>
+							<th>UpdatedAt</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((user, index) => (
+							<tr key={user.id}>
+								<td>{index + 1}</td>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+								<td>{user.address}</td>
+								<td>{user.phone}</td>
+								<td>{user.created_at}</td>
+								<td>{user.updated_at}</td>
+								<td>
+									<Link
+										to={`/user/edit/${user.id}`}
+										className="button is-small is-info mr-2">
+										Edit
+									</Link>
+									<button
+										onClick={() => deleteUser(user.id)}
+										className="button is-small is-danger">
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default UserList;
