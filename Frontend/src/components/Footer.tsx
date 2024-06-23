c79d2254f7ecdb088c723e6dd34461978a6edaa3
Musyaffa Afiful Umam <afzam613@gmail.com>
2024-06-23 22:45:24 +0700

    Update Footer.tsx

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -14,7 +14,7 @@ function Footer() {
 										<p className="white-text">
 											Mengubah plastik menjadi peluang:
 											<br />
-											Satu langkah kecil untuk bumi yang lebih
+											Satu langkah kecil untuk Bumi yang lebih
 											hijau.
 										</p>
 									</li>
